b053c91f0ca6377aa97739103c73dd6beef8dcab
Azuki19 <analucia27259@gmail.com>
2024-03-01 15:12:33 -0500

    feat

---
+++ public/src/componentes/card/card.js
@@ -0,0 +1,32 @@
+class card extends HTMLElement {
+
+	constructor() {
+		super();
+		this.attachShadow({mode: 'open'})
+	}
+}
+
+static get ObservedAttributes (){
+  return ['image', 'name', 'genre'];
+}
+
+connectedCallback(){
+  this.render();
+}
+
+attributeChangedCallback (propName, oldValue, newValue){
+  this[propName]=newValue;
+  this.render();
+}
+
+render(){
+  this.ShadowRoot.innerHTML=`
+  <link rel="stylesheet" href="./src/componentes/card/card.css">
+  <h1>${this.name}</h1>
+  <p>${this.genre}</p>
+  <Image src = ${this.Image}></Image>
+  `;
+}
+
+customElements.define(`card-card`, card);
+export default card;
